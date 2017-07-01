54e5f17aa610f2739408af1967f0ac73236045b2
Dana Johnson <dnjohnson84@gmail.com>
2017-06-30 17:07:30 -0700

    Fix radio input functionality in poem nav

--- _assets/javascripts/navigation.js
+++ _assets/javascripts/navigation.js
@@ -1,30 +1,47 @@
-/*****
-Uncheck radio inputs in poem navigation
-*****/
-
+// ===================================
+// Uncheck radio inputs in poem navigation
+// ===================================
 $(document).ready(function() {
-  $('.tab__label').on('click', function(e){
+  $('.tab__label').on('click', function (e) {
     var inputPressed = $(this).prev('input[type=radio]');
-    if( inputPressed.is(':checked') ) {
+    if(inputPressed.is(':checked')) {
       e.preventDefault();
       inputPressed.prop('checked', false );
     }
   })
 });
 
+// ===================================
+// Toggle tab groups on small and medium screens
+// ===================================
+var tabGroupNavigation = '#tabGroupNavigation';
+var tabGroupTools = '#tabGroupTools';
+
+$('#tabNavigation').on('click', function (e) {
+  tabState(tabGroupTools, 'off');
+  tabState(tabGroupNavigation, 'on', 'off');
+  e.preventDefault();
+});
+
+$('#tabTools').on('click', function (e) {
+  tabState(tabGroupNavigation, 'off');
+  tabState(tabGroupTools, 'on', 'off');
+  e.preventDefault();
+});
+
+// function to switch data atribute state
 var tabState = function (elem, one, two) {
   var elem = document.querySelector(elem);
   elem.setAttribute('data-tab', elem.getAttribute('data-tab') === one ? two : one);
 };
 
-var tabGroupNavigation = '#tabGroupNavigation';
-$('#tabNavigation').on('click', function(e){
-  tabState(tabGroupNavigation, 'on', 'off');
-  e.preventDefault();
+// ===================================
+// Show only one tab group at a time
+// ===================================
+$(tabGroupNavigation).children('input[type=radio]').on('click', function (e) {
+  $(tabGroupTools).children('input[type=radio]').prop('checked', false );
 });
 
-var tabGroupTools = '#tabGroupTools';
-$('#tabTools').on('click', function(e){
-  tabState(tabGroupTools, 'on', 'off');
-  e.preventDefault();
+$(tabGroupTools).children('input[type=radio]').on('click', function (e) {
+  $(tabGroupNavigation).children('input[type=radio]').prop('checked', false );
 });
