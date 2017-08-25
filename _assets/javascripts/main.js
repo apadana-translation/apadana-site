8fc2df39fc6dff3537f570f5e6fdd49465a36fbf
Dana Johnson <dnjohnson84@gmail.com>
2017-08-25 16:13:24 -0700

    Remove smoothState.js

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -2,12 +2,10 @@
 //= require jquery/jquery.min.js
 //= require moment/moment.js
 //= require fetch/fetch.js
-//= require jquery.smoothState.js
 //= require jquery.sidenotes/lib/jquery.sidenotes.js
 
 // Compile user scripts
 //= require ga
-//= require smoothState
 //= require navigation
 //= require progress-bar
 //= require sidenote
