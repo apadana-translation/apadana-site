0ad09a7e8377781e8fecfd6387ae5e9cff67da07
Dana Johnson <dnjohnson84@gmail.com>
2018-03-26 09:26:15 -0700

    Add feedback link to mobile navbar
    Rebuild mobile-pagination to include multiple sections for pagination and feedback. Mobile audio will go here as well.

--- _assets/js/modal.js
+++ _assets/js/modal.js
@@ -1,33 +1,10 @@
-// TODO: DRY this out!
+var modalParent = document.querySelector('body');
 
-var modalParent = document.querySelector('body'),
-    modalOpen = document.querySelector('#modal-toggle'),
-    modalClose = document.querySelector('#modal-close');
-
-modalOpen.onclick = function() {
-  var modalState = this.getAttribute('modal-state');
+$('.modal-open, #modal-close').click(function() {
+  var modalState = modalParent.getAttribute('modal-state');
   if (modalState != 'is-open') {
-    this.setAttribute('modal-state', 'is-open');
     modalParent.setAttribute('modal-state', 'is-open');
-    modalClose.setAttribute('modal-state', 'is-open');
-
   } else {
-    this.setAttribute('modal-state', 'is-closed');
     modalParent.setAttribute('modal-state', 'is-closed');
-    modalClose.setAttribute('modal-state', 'is-closed');
   }
-};
-
-modalClose.onclick = function() {
-  var modalState = this.getAttribute('modal-state');
-  if (modalState != 'is-open') {
-    this.setAttribute('modal-state', 'is-open');
-    modalParent.setAttribute('modal-state', 'is-open');
-    modalOpen.setAttribute('modal-state', 'is-open');
-
-  } else {
-    this.setAttribute('modal-state', 'is-closed');
-    modalParent.setAttribute('modal-state', 'is-closed');
-    modalOpen.setAttribute('modal-state', 'is-closed');
-  }
-};
+});
