f668349fa25db49993c81a14b92b9e4d334ff5e9
Dana Johnson <dnjohnson84@gmail.com>
2018-04-05 16:52:12 -0700

    Turn bibtex urls into custom hyperlinks

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,1 +1,12 @@
 require "breakpoint"
+
+# If jekyll-scholar reference has a url, turn it into a link that matches self-hosted PDF link stying
+# https://github.com/inukshuk/jekyll-scholar/issues/30
+require 'uri'
+module UrlFilter
+  class StripUrls < BibTeX::Filter
+    def apply(value)
+      value.to_s.gsub(URI.regexp(['http','https','ftp'])) { |c| "[<svg class=\"icon-pdf\"><use xlink\:href=\"#icon-pdf\"></use></svg><a href=\"#{$&}\" target=\"_blank\"><abbr>PDF</abbr></a>]" }
+    end
+  end
+end
